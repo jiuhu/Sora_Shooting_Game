9f264e044dff11a29091868cfc4dba021b0265b5
Ewe, YS <aheweys@gmail.com>
2013-03-24 19:37:23 +0800

    basic shoot

--- js/common.js
+++ js/common.js
@@ -15,7 +15,6 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-//  Global Variables
 var TWO_PI = 2 * Math.PI;
 //--------------------------------------------------------------------
 
